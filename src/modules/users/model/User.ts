f68486e4bf73834c0a58082f66a83d75d879111b
Raul Duarte Pereira <raulduartepe@gmail.com>
2021-03-19 20:50:18 -0300

    refactor: tidy app

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,23 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidV4 } from 'uuid';
 
 class User {
-  // Complete aqui
+  public readonly id: string;
+
+  public name: string;
+
+  public admin = false;
+
+  public email: string;
+
+  public created_at: Date;
+
+  public updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
   user_id: string;
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User not exists');
+    }
+
+    if (!user.admin) {
+      throw new Error('User without permission to list all users');
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
